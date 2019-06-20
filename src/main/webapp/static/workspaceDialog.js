b87495e1321f1044309153452085f6a7d85f1ad2
Radislav Berkovich <radislav.berkovich@microfocus.com>
2019-06-20 12:59:49 +0300

    changes

---
+++ src/main/webapp/static/workspaceDialog.js
@@ -0,0 +1,85 @@
+var validateRequiredFieldsFilledAuto = false;
+var customData;
+AP.dialog.disableCloseOnSubmit();
+AP.dialog.getCustomData(function (data) {
+    customData = data;
+    //set header
+    $(".aui-dialog2-header-main").text(customData.header);
+
+    //assign buttons
+    $("#confirm-button").click(function (e) {
+        e.preventDefault();
+        submit();
+    });
+    $("#cancel-button").click(function (e) {
+        e.preventDefault();
+        AP.dialog.close();
+    });
+
+
+    //set fields
+    if (isEditMode()) {
+        //$("#name").val(customData.entity.name);
+        //$("#location").val(customData.entity.location);
+        //$("#clientId").val(customData.entity.clientId);
+        //$("#clientSecret").val(customData.entity.clientSecret);
+    }
+});
+
+function getProperties() {
+    var data = {
+        name: $("#name").attr("value"),
+        location: $("#location").attr("value"),
+        clientId: $("#clientId").attr("value"),
+        clientSecret: $("#clientSecret").attr("value")
+    };
+    if (isEditMode()) {
+        data.id = customData.entity.id;
+    }
+    return data;
+}
+
+function isEditMode() {
+    return customData.editMode && customData.entity;
+}
+
+function validateRequiredFieldsFilled() {
+    if (!validateRequiredFieldsFilledAuto) {
+        validateRequiredFieldsFilledAuto = true;
+        $(".required").change(function () {
+            validateRequiredFieldsFilled();
+        });
+    }
+
+    var validationFailed = !validateMissingRequiredAndUpdateErrorField($("#location").val(), "#locationError");
+    validationFailed = !validateMissingRequiredAndUpdateErrorField($("#name").val(), "#nameError") || validationFailed;
+    validationFailed = !validateMissingRequiredAndUpdateErrorField($("#clientId").val(), "#clientIdError") || validationFailed;
+    validationFailed = !validateMissingRequiredAndUpdateErrorField($("#clientSecret").val(), "#clientSecretError") || validationFailed;
+    return !validationFailed;
+}
+
+
+function submit() {
+    if (!validateRequiredFieldsFilled()) {
+        return;
+    }
+    setStatus("Saving ...");
+    var entityProperties = getProperties();
+    var requestType;
+    var url;
+    if (isEditMode()) {
+        requestType = "PUT";
+        url = "/rest/configuration/spaces/" + entityProperties.id;
+    } else {
+        requestType = "POST";
+        url = "/rest/configuration/spaces";
+    }
+
+    hostAjaxSend(requestType, url, JSON.stringify(entityProperties))
+        .then(function (result) {
+            showFlag('Workspace configuration saved successfully.');
+            AP.dialog.close({entity: result});
+        }).catch(function (error) {
+        setStatus("Failed to save : " + error.message, "failed");
+    });
+}
